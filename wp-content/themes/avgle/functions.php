5f8225c5f59e1b6ce84b016b574d89827a3fbfb4
harveyNorman <wei2215038@gmail.com>
2018-01-18 23:54:48 +1100

    initial commit

---
+++ wp-content/themes/avgle/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * GCG Portal functions and definitions
+ *
+ * @package FoundSolutions
+ * @subpackage GCG_Portal
+ * @since GCG Portal 0.0.1
+ */
+
+//script verison
+global $av_site_version;
+$av_site_version = '3.3.6';
+
+require_once(__DIR__.'/includes/old_function.php');
+
+require_once(__DIR__.'/includes/theme-init.php');
+require_once(__DIR__.'/includes/util.php');
